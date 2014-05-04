8c47cf97727b8d1b2cdfc5090bc246d5060c6e65
vlad <vlad@v-lad.org>
2014-05-03 22:50:27 -0400

    More comprehensive tests for chomp

--- aws4c_test.cpp
+++ aws4c_test.cpp
@@ -1,6 +1,7 @@
 #include <CppUTest/CommandLineTestRunner.h>
 #include <CppUTest/JUnitTestOutput.h>
 #include <stdio.h>
+#include <string.h>
 #include "aws4c.h"
 
 
@@ -20,13 +21,49 @@ extern STATIC "C" void __chomp ( char  * str );
 
 TEST(FirstTestGroup, Chomp_ShouldHandleEmptyString)
 {
-   char * testStr = "";    
-
+   char * testStr = strdup("");    
    __chomp(testStr);
-
    STRCMP_EQUAL("",testStr);
-
 }
+
+TEST(FirstTestGroup, Chomp_ShouldWorkWithNormalString)
+{
+   char * testStr = strdup("Test\n");    
+   __chomp(testStr);
+   STRCMP_EQUAL("Test",testStr);
+}
+
+TEST(FirstTestGroup, Chomp_ShouldHandleCRandNL)
+{
+   char * testStr = strdup("Test\r\n");    
+   __chomp(testStr);
+   STRCMP_EQUAL("Test",testStr);
+}
+
+TEST(FirstTestGroup, Chomp_ShouldNotChokeOnSingleNL)
+{
+   char * testStr = strdup("\n");    
+   __chomp(testStr);
+   STRCMP_EQUAL("",testStr);
+}
+
+TEST(FirstTestGroup, Chomp_ShouldNotChangeStrsWithNoNL)
+{
+   char * testStr = "Hello";    
+   __chomp(testStr);
+   STRCMP_EQUAL("Hello",testStr);
+}
+
+TEST(FirstTestGroup, Chomp_ShouldIgnoreNLNotAtTheEnd)
+{
+   char * testStr = "He\nllo";    
+   __chomp(testStr);
+   STRCMP_EQUAL("He\nllo",testStr);
+}
+
+
+
+
 
 int main(int ac, char** av)
 {
